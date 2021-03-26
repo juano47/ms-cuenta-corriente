9f7ec26ab25ad4d4ef20eff0f3089bcaea01799a
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2021-03-25 21:46:36 -0300

    Initial commit

---
+++ src/main/java/ms/cuenta/corriente/MsCuentaCorrienteApplication.java
@@ -0,0 +1,13 @@
+package ms.cuenta.corriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCuentaCorrienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCuentaCorrienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ms/cuenta/corriente/MsCuentaCorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.cuenta.corriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCuentaCorrienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
