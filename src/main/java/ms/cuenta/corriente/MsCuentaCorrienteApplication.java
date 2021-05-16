5bfbcd49d3abc0659fa458269c5f7bfc843566e6
sfrigieri <santiagofrigieri@outlook.com>
2021-05-16 05:15:01 -0300

    + LogAspect

--- src/main/java/ms/cuenta/corriente/MsCuentaCorrienteApplication.java
+++ src/main/java/ms/cuenta/corriente/MsCuentaCorrienteApplication.java
@@ -2,8 +2,10 @@ package ms.cuenta.corriente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class MsCuentaCorrienteApplication {
 
     public static void main(String[] args) {
